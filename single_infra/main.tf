249d51638e17635af9be8e0eb24e2bdc6061698b
Nnamdi92 <echezonannamdi@gmail.com>
2025-07-14 11:42:04 +0100

    initial commit

---
+++ single_infra/main.tf
@@ -0,0 +1,17 @@
+resource "google_storage_bucket" "sales_bucket" {
+  name          = var.bucket_name
+  location      = var.bucket_location
+  force_destroy = true
+  public_access_prevention = var.public_access_prevention
+
+  lifecycle_rule {
+    condition {
+      age = var.lifecycle_rule_age
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  
+}
